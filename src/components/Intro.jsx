1cf33023453d7b64b2af5724db0293e765691843
chloe iy choi <ioiuioiciy88@gmail.com>
2023-08-16 23:49:06 +0900

    인트로 완료

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+import about from "../assets/img/about.jpg";
+
+const introText = {
+    title: "MARKUP DEVELOPER",
+    desc: ["TALENT IS", "FOUND AT THE END OF THE", "EFFORT"],
+};
+
 const Intro = () => {
-    return <div>Intro</div>;
+    return (
+        <section id="intro">
+            <div class="intro__inner">
+                <h1 class="intro__title">{introText.title}</h1>
+                <div class="intro__lines" aria-hidden="true">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </div>
+                <div class="intro__text">
+                    <div class="text">
+                        <div>{introText.desc[0]}</div>
+                        <div>{introText.desc[1]}</div>
+                        <div>{introText.desc[2]}</div>
+                    </div>
+                    <div class="img">
+                        <img src={about} alt="어바웃" />
+                    </div>
+                </div>
+                <div class="intro__lines bottom" aria-hidden="true">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Intro;
