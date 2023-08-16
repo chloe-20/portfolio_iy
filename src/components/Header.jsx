36941a736d25061d5e07f6d4cf4ed08357e98d0d
chloe iy choi <ioiuioiciy88@gmail.com>
2023-08-16 23:18:39 +0900

    헤더영역 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+
+const headerNav = [
+  {
+    title: "INTRO",
+    url: "#intro"
+  },
+  {
+    title: "ABOUT ME",
+    url: "#aboutMe"
+  },
+  {
+    title: "EXPERIENCE",
+    url: "#experience"
+  },
+  {
+    title: "WORK",
+    url: "#work"
+  },
+  {
+    title: "CONTACT",
+    url: "#contact"
+  }
+];
 
 const Header = () => {
-    return <div>Header</div>;
+  const [show, setShow] = useState(false);
+
+  const toggleMenu = () => {
+    setShow((prevShow) => !prevShow);
+  };
+
+  return (
+    <header id="header" role="banner">
+      <div className="header__inner">
+        <div className="header__logo">
+          <a href="/">PORTFOLIO<em>CHLOE_CHOI</em></a>
+        </div>
+        <nav className="header__nav" role="navigation" aria-label="메인 메뉴">
+          <ul>
+          {headerNav.map((nav, key) => (
+            <li key={key}>
+              <a href={nav.url}>{nav.title}</a>
+            </li>
+          ))}
+          </ul>
+        </nav>
+        <div 
+          className="header__nav__mobile"
+          id="headerToggle"
+          aria-controls="primary-menu"
+          aria-expanded={show ? "true" : "false"}
+          role="button" 
+          tabIndex="0"
+          onClick={toggleMenu}
+        >
+          <span></span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
