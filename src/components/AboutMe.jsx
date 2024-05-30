e685cb8ea0b00b5c782e0191a12a5dd83a0a77aa
chloe iy choi <ioiuioiciy88@gmail.com>
2024-05-30 22:22:07 +0900

    portfolio update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,59 +1,70 @@
 import React from "react";
 
 const AboutMe = () => {
-    return (
-        <section id="aboutMe">
-            <div className="aboutMe__inner">
-                <h2 className="aboutMe__title">ABOUT ME</h2>
-                <div className="aboutMe__desc">
-                    <div>
-                        <span className="num">1.</span>
-                        <h3>보고, 듣고, 고민하는 웹 퍼블리셔 최인영</h3>
-                        <p>
-                            <em className="highlight">Seeing</em><br/>
-                            <em className="highlight">Listening</em><br/>
-                            <em className="highlight">Thinking</em><br/><br/>
-                            각양각색의 삶을 살아온 타인과 대화할 때 얻는 다양한 생각과 의견을 경청하길 좋아합니다. 경청하는 습관은 새로운 것을 배우고 적용하며 배움의 넓이를 확장시켜 제 삶을 풍요롭게 만들어 주었습니다. 2년 동안 운영, 구축, 개선 등 다양한 프로젝트를 수행하면서 동료들과 긴밀히 소통하며 좋은 결과물을 만들어 내기 위해 노력하였습니다. 그 과정 가운데 다양한 지식을 배울 수 있었고, 한층 더 성장 할 수 있었습니다. 도전과 성장을 추구하는 웹 퍼블리셔로서 더욱 전문성을 갖춰, 다양한 사용자들에게 더 나은 경험을 제공하는 것을 목표로 더 열심히 나아가겠습니다.
-                        </p>
-                    </div>
-                    <div>
-                        <span className="num">2.</span>
-                        <h3>I work with these Skill</h3>
-                        <p>
-                            <em className="highlight">HTML5</em>
-                            <em className="highlight">CSS3</em>
-                            <em className="highlight">SCSS</em>
-                            <em className="highlight">Javascript</em>
-                            <em className="highlight">Jquery</em>
-                            <em className="highlight">SVN</em>
-                            <em className="highlight">Figma</em>
-                            <em className="highlight">Zeplin</em>
-                            <em className="highlight">Photoshop</em>
-                            <em className="highlight">Slack</em>
-                            <em className="highlight">CMS</em><br/><br/>
-                            And&nbsp;&nbsp;
-                            <em className="highlight">긍정적</em>
-                            <em className="highlight">끈기</em>
-                            <em className="highlight">책임감</em>
-                            <em className="highlight">협동심</em>
-                            <em className="highlight">계획성</em>
-                        </p>
-                    </div>
-                    <div>
-                        <span className="num">3.</span>
-                        <h3>I worked at</h3>
-                        <ol className="timeline">
-                            <li>
-                                <p>
-                                    Web Agency <em className="period">2021 - 2023</em>
-                                </p>
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section id="aboutMe">
+      <div className="aboutMe__inner">
+        <h2 className="aboutMe__title">ABOUT ME</h2>
+        <div className="aboutMe__desc">
+          <div>
+            <span className="num">1.</span>
+            <h3>보고, 듣고, 고민하는 웹 퍼블리셔 최인영</h3>
+            <p>
+              <em className="highlight">Seeing</em>
+              <br />
+              <em className="highlight">Listening</em>
+              <br />
+              <em className="highlight">Thinking</em>
+              <br />
+              <br />
+              각양각색의 삶을 살아온 타인과 대화할 때 얻는 다양한 생각과 의견을 경청하길 좋아합니다. 경청하는 습관은 새로운 것을 배우고 적용하며 배움의 넓이를 확장시켜 제 삶을 풍요롭게 만들어 주었습니다. 2년 동안 운영, 구축, 개선 등 다양한 프로젝트를 수행하면서 동료들과 긴밀히 소통하며 좋은 결과물을 만들어 내기 위해 노력하였습니다. 그 과정 가운데 다양한 지식을 배울 수 있었고, 한층 더 성장 할 수 있었습니다. 도전과 성장을 추구하는 웹 퍼블리셔로서 더욱 전문성을 갖춰, 다양한 사용자들에게 더 나은 경험을 제공하는 것을 목표로 더 열심히 나아가겠습니다.
+            </p>
+          </div>
+          <div>
+            <span className="num">2.</span>
+            <h3>I work with these Skill</h3>
+            <p>
+              <em className="highlight">HTML5</em>
+              <em className="highlight">CSS3</em>
+              <em className="highlight">SCSS</em>
+              <em className="highlight">Javascript</em>
+              <em className="highlight">Jquery</em>
+              <em className="highlight">SVN</em>
+              <em className="highlight">Figma</em>
+              <em className="highlight">Zeplin</em>
+              <em className="highlight">Photoshop</em>
+              <em className="highlight">Slack</em>
+              <em className="highlight">CMS</em>
+              <br />
+              <br />
+              And&nbsp;&nbsp;
+              <em className="highlight">긍정적</em>
+              <em className="highlight">끈기</em>
+              <em className="highlight">책임감</em>
+              <em className="highlight">협동심</em>
+              <em className="highlight">계획성</em>
+            </p>
+          </div>
+          <div>
+            <span className="num">3.</span>
+            <h3>I worked at</h3>
+            <ol className="timeline">
+              <li>
+                <p>
+                  Web Agency, freelancer <em className="period">2023.11 - 2024.05</em>
+                </p>
+              </li>
+              <li>
+                <p>
+                  Web Agency <em className="period">2021 - 2023</em>
+                </p>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AboutMe;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,35 +1,36 @@
-import work01 from "../assets/img/port01.jpg";
-import work02 from "../assets/img/port02.jpg";
-import work03 from "../assets/img/port03.jpg";
-import work04 from "../assets/img/port04.jpg";
-import work05 from "../assets/img/port05.jpg";
-import work06 from "../assets/img/port06.jpg";
+import work01 from "../assets/img/port07.jpg";
+import work02 from "../assets/img/port01.jpg";
+import work03 from "../assets/img/port02.jpg";
+import work04 from "../assets/img/port03.jpg";
+import work05 from "../assets/img/port04.jpg";
+import work06 from "../assets/img/port05.jpg";
+import work07 from "../assets/img/port06.jpg";
 
 export const headerNav = [
   {
     title: "INTRO",
-    url: "#intro"
+    url: "#intro",
   },
   {
     title: "ABOUT ME",
-    url: "#aboutMe"
+    url: "#aboutMe",
   },
   {
     title: "EXPERIENCE",
-    url: "#experience"
+    url: "#experience",
   },
   {
     title: "WORK",
-    url: "#work"
+    url: "#work",
   },
   {
     title: "CONTACT",
-    url: "#contact"
-  }
+    url: "#contact",
+  },
 ];
 
 export const introText = {
-  title: ["I AM","MARKUP DEVELOPER"],
+  title: ["I AM", "MARKUP DEVELOPER"],
   desc: ["SLOW AND STEADY", "JUST KEEP GOING"],
 };
 
@@ -38,68 +39,76 @@ export const experienceText = [
     period: "2021.04 - 2021.09",
     text: ["멀티미디어콘텐츠 제작(웹&영상)", "방송정보국제교육원(IIBI)"],
     title: "멀티미디어콘텐츠(웹&영상) 제작 교육 과정 수료",
-    info: [
-      "PERIOD : 6 MONTHS",
-      "USE STACK : HTML5/CSS3, JAVASCRIPT, JQUERY, PHOTOSHOP, ILLUSTRATOR, PREMIERE PRO, AFTER EFFECTS, CINEMA 4D",
-    ],
+    info: ["PERIOD : 6 MONTHS", "USE STACK : HTML5/CSS3, JAVASCRIPT, JQUERY, PHOTOSHOP, ILLUSTRATOR, PREMIERE PRO, AFTER EFFECTS, CINEMA 4D"],
   },
   {
     period: "2021.09 - 2023.08",
     text: ["웹 에이전시", "UI연구개발부, 주임"],
     title: "운영, 구축, 개선 등 다양한 프로젝트 수행",
-    info: [
-      "PERIOD : 2 YEARS",
-      "USE STACK : HTML5/CSS3, JAVASCRIPT, JQUERY, PHOTOSHOP, FIGMA, ZEPLIN, SLACK",
-    ],
+    info: ["PERIOD : 2 YEARS", "USE STACK : HTML5/CSS3, JAVASCRIPT, JQUERY, PHOTOSHOP, FIGMA, ZEPLIN, SLACK"],
+  },
+  {
+    period: "2023.11 - 2024.05",
+    text: ["웹 에이전시", "프리랜서 퍼블리셔"],
+    title: "케이뱅크 접근성 개선 프로젝트 수행(기업뱅킹 담당)",
+    info: ["PERIOD : 7 MONTHS", "USE STACK : HTML5/CSS3, JAVASCRIPT, JQUERY"],
   },
 ];
 
 export const workText = [
   {
     num: "01",
+    title: "케이뱅크 접근성 개선 프로젝트",
+    desc: "운영되고 있는 케이뱅크 기업뱅킹 웹/앱 페이지에 접근성 속성을 반영하여 접근성을 개선하였습니다.",
+    img: work01,
+    view: "https://biz.kbanknow.com/ib20/mnu/CBKCSC020100",
+    name: "케이뱅크 기업뱅킹 웹/앱",
+  },
+  {
+    num: "02",
     title: "포트폴리오 사이트(2023)",
     desc: "개인 포트폴리오 사이트 2023 리액트 버전입니다.",
-    img: work01,
+    img: work02,
     view: "https://github.com/chloe-20/portfolio_iy",
     name: "2023 리액트로 만든 포트폴리오",
   },
   {
-    num: "02",
+    num: "03",
     title: "하나원큐 회원가입개편",
     desc: "HTML, CSS, Javascript, JQuery를 활용하여 페이지 구축했습니다.",
-    img: work02,
+    img: work03,
     view: "https://www.hanabank.com/oneQp/index.html#none",
     name: "하나원큐 앱",
   },
   {
-    num: "03",
+    num: "04",
     title: "한경 아르테 구축",
     desc: "HTML, CSS, Javascript, JQuery를 활용하여 페이지 구축했습니다.",
-    img: work03,
+    img: work04,
     view: "https://www.arte.co.kr/",
     name: "한경 아르떼 웹 사이트",
   },
   {
-    num: "04",
+    num: "05",
     title: "삼성 언팩 2022",
     desc: "AEM 프로그램을 활용하여 국가별 출시 예정인 제품 관련 데이터 입력 및 배포하였습니다.",
-    img: work04,
+    img: work05,
     view: "https://www.samsung.com/sec/smartphones/galaxy-s23/",
     name: "삼성 갤럭시 S23 웹 사이트",
   },
   {
-    num: "05",
+    num: "06",
     title: "LG CNS MOP 광고플랫폼 구축",
     desc: "HTML, CSS, Javascript, JQuery를 활용하여 페이지 구축했습니다.",
-    img: work05,
+    img: work06,
     view: "https://mop.co.kr/intro-page/index.html",
     name: "LG CNS MOP 광고플랫폼 웹 사이트",
   },
   {
-    num: "06",
+    num: "07",
     title: "MG기업뱅킹(인터넷/스마트) 구축",
     desc: "HTML, CSS, Javascript, JQuery를 활용하여 페이지 구축했습니다.",
-    img: work06,
+    img: work07,
     view: "https://biz.kfcc.co.kr/",
     name: "MG기업뱅킹 웹 사이트",
   },
